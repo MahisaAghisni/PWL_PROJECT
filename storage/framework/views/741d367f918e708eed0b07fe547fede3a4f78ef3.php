dc18be35ef5ddb38f4f7984ebecf5a4b4b6af494
MahisaAghisni <aghisnifadhli@gmail.com>
2022-06-05 22:23:14 +0700

    update gambar

---
+++ storage/framework/views/741d367f918e708eed0b07fe547fede3a4f78ef3.php
@@ -0,0 +1,53 @@
+<?php $__env->startSection('content'); ?>
+    <div class="container-fluid">
+
+        <!-- Page Heading -->
+        <?php if($errors->any()): ?>
+            <div class="alert alert-danger">
+                <ul>
+                    <?php $__currentLoopData = $errors->all(); $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $error): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                        <li><?php echo e($error); ?></li>
+                    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                </ul>
+            </div>
+        <?php endif; ?>
+
+        <!-- Content Row -->
+        <div class="card shadow">
+            <div class="card-header">
+                <div class="d-sm-flex align-items-center justify-content-between mb-4">
+                    <h1 class="h3 mb-0 text-gray-800"><?php echo e(__('create arena')); ?></h1>
+                    <a href="/arena" class="btn btn-primary btn-sm shadow-sm">Go Back</a>
+                </div>
+            </div>
+            <div class="card-body">
+                <form action="<?php echo e(route('arena.store')); ?>" method="POST" enctype="multipart/form-data">
+                    <?php echo csrf_field(); ?>
+                    
+                    <div class="form-group">
+                        <label for="price">Harga</label>
+                        <input type="text" name="price" class="form-control" id="price" aria-describedby="price">
+                    </div>
+                    <div class="form-group">
+                        <label for="image">gambar</label>
+                        <input type="file" class="form-control" required="required" name="image">
+                    </div>
+                    <div class="form-group">
+                        <label for="status">Status</label>
+                        <select name="status" id="status" class="form-control">
+                            <option value="1">Active</option>
+                            <option value="0">In Active</option>
+                        </select>
+                    </div>
+                    <button type="submit" class="btn btn-primary btn-block">Submit</button>
+                </form>
+            </div>
+        </div>
+
+
+        <!-- Content Row -->
+
+    </div>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.main', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\PWL_PROJECT\resources\views/admin/arenas/create.blade.php ENDPATH**/ ?>
