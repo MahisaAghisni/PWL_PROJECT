6b2b959923abccdd6e6874e4d4ea518c30937b8a
MahisaAghisni <aghisnifadhli@gmail.com>
2022-06-08 11:05:38 +0700

    menambahkan table jenis

---
+++ app/Models/Jenis.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Jenis extends Model
+{
+    use HasFactory;
+
+    protected $table = 'jenis'; // Eloquent akan membuat model mahasiswa menyimpan record di tabel jenis
+    public $timestamps = false;
+    protected $primaryKey = 'id'; // Memanggil isi DB Dengan primarykey
+
+
+    protected $fillable = [
+        'id',
+        'nama',
+        'deskripsi',
+    ];
+}
